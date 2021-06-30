9be646d4ab19b56835f0b4962783331cfe8cbc78
Dongre-Hemang <84710644+Dongre-Hemang@users.noreply.github.com>
2021-06-30 15:49:00 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,1 +1,13 @@
+void swap_max(int arr[], int l, int n){
+    int max=arr[n],p=n;
+    for(int i=n;i<l;i++){
+        if(arr[i]>max){
+            max=arr[i];
+            p=i;
+        }
+        
+    }
+    arr[p]=arr[n];
+    arr[n]=max;
+}
 
